a67dd6ac5efca1126782eeafd9d2187f9bf86845
Sagar Singh <sssssagarsingh24@gmail.com>
2024-11-08 00:06:21 +0530

    Add files via upload

---
+++ Conditional_Statement/if_Else01.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    if(n>0){
+        cout<<"A is positive"<<endl;
+    }
+}
---
+++ Conditional_Statement/if_Else04.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter the value of a: ";
+    cin>>a;
+    if(a>0){
+        cout<<"A is positive"<<endl;
+    }
+    else{
+        cout<<"A is negative"<<endl;
+    }
+}
