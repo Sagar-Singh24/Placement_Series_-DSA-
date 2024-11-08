7baa0322e3ee39310ef36b9c2ae84fbc33639bbe
Sagar Singh <sssssagarsingh24@gmail.com>
2024-11-09 01:20:41 +0530

    Add files via upload

---
+++ Loops/Loop01.cpp
@@ -0,0 +1,14 @@
+//Printing number
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        cout<<i<<" ";
+        i= i+1;
+    }
+}
