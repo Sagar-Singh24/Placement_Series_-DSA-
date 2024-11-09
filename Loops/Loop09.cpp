1b8478f479549fcd8abb187762fb06ec62a748e6
Sagar Singh <sssssagarsingh24@gmail.com>
2024-11-10 01:03:46 +0530

    Add files via upload

---
+++ Loops/Loop09.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+     int n;
+    cout<<"Enter the value of n:"<<endl;
+    cin>>n;
+
+    for(int i=1; i<=n; i++){
+        cout<<"HI"<<endl;
+        cout<<"Hey"<<endl;
+        continue;
+        cout<<"Reply me"<<endl;
+    }
+   
+}
