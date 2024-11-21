4ddb0f8aba2d81b17287ccbd99358729f3f34f02
Sagar Singh <sssssagarsingh24@gmail.com>
2024-11-22 01:05:22 +0530

    Add files via upload

---
+++ Array/Array04.cpp
@@ -0,0 +1,22 @@
+//Array Scope
+
+#include<iostream>
+using namespace std;
+
+void update(int arr[],int n){
+    cout<<endl<<"Inside the function"<<endl;
+    for(int i=0; i<3; i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+  cout<<"Going back to main function"<<endl;
+}
+
+int main(){
+    int arr[3]={1,2,3};
+   update(arr,3);
+   cout<<endl<<"Printing in main function"<<endl;
+   for(int i=0; i<3; i++){
+    cout<<arr[i]<<" ";
+   }
+    cout<<endl;
+}
