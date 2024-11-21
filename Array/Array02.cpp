527fc3e577a19788685cac8566422a26d043460c
Sagar Singh <sssssagarsingh24@gmail.com>
2024-11-22 01:04:42 +0530

    Add files via upload

---
+++ Array/Array02.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int third[15]={2,7};
+    int n = 15;
+    cout<<"Printing the array"<<endl;
+
+    for(int i=0; i<n; i++)
+    cout<<third[i]<<" ";
+    return 0;
+}
